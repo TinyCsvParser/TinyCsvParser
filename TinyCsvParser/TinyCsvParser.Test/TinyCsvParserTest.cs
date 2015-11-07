276e63e42b76859702f78c9fecaa103b57147aa6
Philipp Wagner <bytefish@gmx.de>
2015-11-07 15:55:23 +0100

    Added a Unit Test
    Unit Test for WithCustomConverter Extension.

--- TinyCsvParser/TinyCsvParser.Test/TinyCsvParserTest.cs
+++ TinyCsvParser/TinyCsvParser.Test/TinyCsvParserTest.cs
@@ -25,15 +25,36 @@ namespace TinyCsvParser.Test
         private class CsvPersonMapping : CsvMapping<Person>
         {
             public CsvPersonMapping()
+            {
+                MapProperty(0, x => x.FirstName);
+                MapProperty(1, x => x.LastName);
+                MapProperty(2, x => x.BirthDate);
+            }
+        }
+
+        private class CsvPersonMappingWithTypeConverterProvider : CsvMapping<Person>
+        {
+            public CsvPersonMappingWithTypeConverterProvider()
                 : this(new TypeConverterProvider())
             {
             }
-            public CsvPersonMapping(ITypeConverterProvider typeConverterProvider)
+            public CsvPersonMappingWithTypeConverterProvider(ITypeConverterProvider typeConverterProvider)
                 : base(typeConverterProvider)
             {
                 MapProperty(0, x => x.FirstName);
                 MapProperty(1, x => x.LastName);
                 MapProperty(2, x => x.BirthDate);
+            }
+        }
+
+        private class CsvPersonMappingWithCustomConverter : CsvMapping<Person>
+        {
+            public CsvPersonMappingWithCustomConverter()
+            {
+                MapProperty(0, x => x.FirstName);
+                MapProperty(1, x => x.LastName);
+                MapProperty(2, x => x.BirthDate)
+                    .WithCustomConverter(new DateTimeConverter("yyyy###MM###dd"));
             }
         }
 
@@ -152,14 +173,14 @@ namespace TinyCsvParser.Test
         }
 
         [Test]
-        public void WeirdDateTimeTest()
+        public void WeirdDateTimeTest_ProviderBased()
         {
             // Instantiate the TypeConverterProvider and override the DateTimeConverter:
             ITypeConverterProvider csvTypeConverterProvider =
                 new TypeConverterProvider().Add(new DateTimeConverter("yyyy###MM###dd"));
 
             // Make sure you pass the custom provider into the mapping!
-            CsvPersonMapping csvMapper = new CsvPersonMapping(csvTypeConverterProvider);
+            var csvMapper = new CsvPersonMappingWithTypeConverterProvider(csvTypeConverterProvider);
 
             // And this is business as usual!
             CsvParserOptions csvParserOptions = new CsvParserOptions(true, new[] { ';' });
@@ -182,6 +203,30 @@ namespace TinyCsvParser.Test
             Assert.AreEqual(12, result[0].Result.BirthDate.Day);
         }
 
+        [Test]
+        public void WeirdDateTimeTest_CustomConverterBased()
+        {
+            CsvParserOptions csvParserOptions = new CsvParserOptions(true, new[] { ';' });
+            CsvReaderOptions csvReaderOptions = new CsvReaderOptions(new[] { Environment.NewLine });
+            CsvPersonMappingWithCustomConverter csvMapper = new CsvPersonMappingWithCustomConverter();
+            CsvParser<Person> csvParser = new CsvParser<Person>(csvParserOptions, csvMapper);
+
+            var stringBuilder = new StringBuilder()
+                .AppendLine("FirstName;LastName;BirthDate")
+                .AppendLine("Philipp;Wagner;1986###05###12");
+
+            var result = csvParser
+                .ReadFromString(csvReaderOptions, stringBuilder.ToString())
+                .ToList();
+
+            Assert.AreEqual("Philipp", result[0].Result.FirstName);
+            Assert.AreEqual("Wagner", result[0].Result.LastName);
+
+            Assert.AreEqual(1986, result[0].Result.BirthDate.Year);
+            Assert.AreEqual(5, result[0].Result.BirthDate.Month);
+            Assert.AreEqual(12, result[0].Result.BirthDate.Day);
+        }
+        
         [Test]
         public void ParallelLinqTest()
         {
