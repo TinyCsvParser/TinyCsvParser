f84f909b405ec5c85d3cb5673f647af999d2810e
Philipp Wagner <bytefish@gmx.de>
2015-12-14 20:04:05 +0100

    Nullable Bool

---
+++ TinyCsvParser/TinyCsvParser.Test/TypeConverter/NullableBoolConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using TinyCsvParser.TypeConverter;
+
+namespace TinyCsvParser.Test.TypeConverter
+{
+    [TestFixture]
+    public class NullableBoolConverterTest : BaseConverterTest<bool?>
+    {
+        protected override ITypeConverter<bool?> Converter
+        {
+            get { return new NullableBoolConverter(); }
+        }
+
+        protected override Tuple<string, bool?>[] SuccessTestData
+        {
+            get
+            {
+                return new[] {
+                    MakeTuple("true", true),
+                    MakeTuple("false", false),
+                    MakeTuple(null, default(bool?)),
+                    MakeTuple(string.Empty, default(bool?)),
+                };
+            }
+        }
+
+        protected override string[] FailTestData
+        {
+            get { return new[] { "a" }; }
+        }
+    }
+}
