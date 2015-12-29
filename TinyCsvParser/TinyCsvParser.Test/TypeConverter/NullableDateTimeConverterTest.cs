a750131aaf2162eb5081025897cbc6bb73d66d88
Philipp Wagner <bytefish@gmx.de>
2015-12-29 10:52:24 +0100

    Unit Tests

---
+++ TinyCsvParser/TinyCsvParser.Test/TypeConverter/NullableDateTimeConverterTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using TinyCsvParser.TypeConverter;
+
+namespace TinyCsvParser.Test.TypeConverter
+{
+    [TestFixture]
+    public class NullableDateTimeConverterTest : BaseConverterTest<DateTime?>
+    {
+        protected override ITypeConverter<DateTime?> Converter
+        {
+            get { return new NullableDateTimeConverter(); }
+        }
+
+        protected override Tuple<string, DateTime?>[] SuccessTestData
+        {
+            get
+            {
+                return new[] {
+                    MakeTuple("2014/01/01", new DateTime(2014, 1, 1)),
+                    MakeTuple("9999/12/31", new DateTime(9999, 12, 31)),
+                    MakeTuple(" ", default(DateTime?)),
+                    MakeTuple(null, default(DateTime?)),
+                    MakeTuple(string.Empty, default(DateTime?))
+                };
+            }
+        }
+
+        protected override string[] FailTestData
+        {
+            get { return new[] { "a", "10000/01/01", "1753/01/32", "0/0/0" }; }
+        }
+    }
+}
