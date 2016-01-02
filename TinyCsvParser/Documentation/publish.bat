5713d776bd6f1774d0b707a656780b9e063c6004
Philipp Wagner <bytefish@gmx.de>
2016-01-02 13:16:15 +0100

    [Issue #6] Batch File to automate publishing
    A Batch file has been added to automate the process of publishing the
    documentation. The script switches into the gh-pages branch, merges all
    changes done in the master documentation and then pushes these changes.
    Nice Workflow.

---
+++ TinyCsvParser/Documentation/publish.bat
@@ -0,0 +1,10 @@
+cd %~dp0
+
+:: Switch to GitHub Pages branch:
+git checkout gh-pages
+:: Merge the changes from the master branch:
+git merge -X theirs -X subtree=TinyCsvParser/Documentation/build/html master
+:: And publish online:
+git push origin gh-pages
+
+pause
