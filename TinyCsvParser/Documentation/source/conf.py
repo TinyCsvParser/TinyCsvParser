9da4864c7b11d9a59566f514f40868522cc26301
Philipp Wagner <bytefish@gmx.de>
2016-01-03 16:50:04 +0100

    [Issue #6] Adding .nojekyll file
    I have added a folder extras to the sources of the documentation and
    created a .nojekyll file in it. Then I have pointed the html_extra_path,
    to it, so GitHub pages do not assume we are using Jekyll (makes folders
    with underscores unusable).

--- TinyCsvParser/Documentation/source/conf.py
+++ TinyCsvParser/Documentation/source/conf.py
@@ -133,7 +133,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+html_extra_path = ['extras/.nojekyll']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
