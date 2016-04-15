d5a55a2825aa08fd12b50e014bc24fec9df9748a
Philipp Wagner <bytefish@gmx.de>
2016-04-15 20:30:19 +0200

    New Features
    Adding a FixedLengthTokenizer, adding a PostProcessingDecorator, and
    switching Test Project to NUnit 3.

---
+++ TinyCsvParser/TinyCsvParser.Test/Tokenizer/Decorators/PostProcessingTokenizerDecoratorTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+using System;
+using System.Text;
+using TinyCsvParser.Tokenizer;
+using TinyCsvParser.Tokenizer.Decorators;
+using ColumnDefinition = TinyCsvParser.Tokenizer.FixedLengthTokenizer.ColumnDefinition;
+
+namespace TinyCsvParser.Test.Tokenizer
+{
+    [TestFixture]
+    public class PostProcessingTokenizerDecoratorTests
+    {
+        [Test]
+        public void Tokenize_With_Trim_Test()
+        {
+            ColumnDefinition[] columns = new[] {
+                new FixedLengthTokenizer.ColumnDefinition(0, 10),
+                new FixedLengthTokenizer.ColumnDefinition(10, 20),
+            };
+
+            Func<string, string>  processor = (s) => s.Trim();
+
+            ITokenizer decoratedTokenizer = new FixedLengthTokenizer(columns);
+
+            ITokenizer tokenizer = new PostProcessingTokenizerDecorator(decoratedTokenizer, processor);
+            
+            string input = new StringBuilder()
+                .AppendLine(" Philipp   Wagner   ")
+                .ToString();
+
+            string[] result = tokenizer.Tokenize(input);
+
+            Assert.AreEqual("Philipp", result[0]);
+            Assert.AreEqual("Wagner", result[1]);
+        }
+     }
+}
---
+++ TinyCsvParser/TinyCsvParser.Test/Tokenizer/FixedLengthTokenizerTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+using System.Text;
+using TinyCsvParser.Tokenizer;
+
+namespace TinyCsvParser.Test.Tokenizer
+{
+    [TestFixture]
+    public class FixedLengthTokenizerTests
+    {
+        [Test]
+        public void Tokenize_Line_Test()
+        {
+            var columns = new[] {
+                new FixedLengthTokenizer.ColumnDefinition(0, 10),
+                new FixedLengthTokenizer.ColumnDefinition(10, 20),
+            };
+
+            var tokenizer = new FixedLengthTokenizer(columns);
+            
+            var input = new StringBuilder()
+                .AppendLine("Philipp   Wagner    ")
+                .ToString();
+
+            var result = tokenizer.Tokenize(input);
+
+            Assert.AreEqual("Philipp   ", result[0]);
+            Assert.AreEqual("Wagner    ", result[1]);
+        }
+     }
+}
