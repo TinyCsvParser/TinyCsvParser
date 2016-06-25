bea54ecba92186ae2ee7e5459db1aaf8f02874d4
Philipp Wagner <bytefish@gmx.de>
2016-06-25 09:45:07 +0200

    Issue #9 Token and TokenType
    Initial Commit for RFC4180-conform parsing of CSV data.

---
+++ TinyCsvParser/TinyCsvParser/Tokenizer/Rfc4180/Token.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyCsvParser.Tokenizer.Rfc4180
+{
+    public class Token
+    {
+        public readonly TokenTypeEnum Type;
+
+        public readonly string Content;
+
+        public Token(TokenTypeEnum type)
+            : this(type, string.Empty)
+        {
+        }
+
+        public Token(TokenTypeEnum type, String content)
+        {
+            Type = type;
+            Content = content;
+        }
+    }
+}
---
+++ TinyCsvParser/TinyCsvParser/Tokenizer/Rfc4180/TokenTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyCsvParser.Tokenizer.Rfc4180
+{
+    public enum TokenTypeEnum
+    {
+        Invalid,
+        Token,
+        Comment
+    }
+}
