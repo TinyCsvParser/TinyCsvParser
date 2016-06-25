8e063b909be18048714dabfdc9101c555818357d
Philipp Wagner <bytefish@gmx.de>
2016-06-25 09:59:12 +0200

    Issue #9 Empty Tokenizer and Test
    Adding a new Test for the Tokenizer, which checks if double quoted data
    is retained.

---
+++ TinyCsvParser/TinyCsvParser.Test/Tokenizer/Rfc4180TokenizerTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+using TinyCsvParser.Tokenizer.Rfc4180;
+
+namespace TinyCsvParser.Test.Tokenizer
+{
+    [TestFixture]
+    public class Rfc4180TokenizerTest
+    {
+        public void Rfc4180_QuotedString_Test()
+        {
+            var tokenizer = new Rfc4180Tokenizer();
+
+            // Initialize a String with Double Quoted Data:
+            var line = "\"Michael, Chester\",24,\"Also goes by \"\"Mike\"\", among friends that is\"";
+
+            // Split the Line into its Tokens:
+            var tokens = tokenizer.Tokenize(line);
+
+            // And make sure the Quotes are retained:
+            Assert.AreEqual("Michael, Chester", tokens[0]);
+            Assert.AreEqual("24", tokens[1]);
+            Assert.AreEqual("Also goes by \"\"Mike\"\", among friends that is", tokens[2]);
+        }
+
+    }
+}
---
+++ TinyCsvParser/TinyCsvParser/Tokenizer/Rfc4180/Rfc4180Tokenizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyCsvParser.Tokenizer.Rfc4180
+{
+    public class Rfc4180Tokenizer : ITokenizer
+    {
+        
+        public string[] Tokenize(string input)
+        {
+            return null;
+        }
+    }
+}
