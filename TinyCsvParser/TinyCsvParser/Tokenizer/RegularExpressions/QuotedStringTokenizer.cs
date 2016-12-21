672be723075c3c75da1540b357e2874dde45541f
Philipp Wagner <bytefish@gmx.de>
2016-12-21 13:30:37 +0100

    #17 Replacing the QuotedStringTokenizer
    I have finally replaced the QuotedStringTokenizer with the RFC4180
    Tokenizer, because parsing CSV Data should happen RFC4180-compliant.
    This also resolves #17, added a Unit Test to show the usage.

--- TinyCsvParser/TinyCsvParser/Tokenizer/RegularExpressions/QuotedStringTokenizer.cs
+++ TinyCsvParser/TinyCsvParser/Tokenizer/RegularExpressions/QuotedStringTokenizer.cs
@@ -1,33 +1,20 @@
 ﻿// Copyright (c) Philipp Wagner. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Text.RegularExpressions;
+using TinyCsvParser.Tokenizer.RFC4180;
 
 namespace TinyCsvParser.Tokenizer.RegularExpressions
 {
-    public class QuotedStringTokenizer : RegularExpressionTokenizer
+    public class QuotedStringTokenizer : RFC4180Tokenizer
     {
-        private Regex regexp;
-
-        public override Regex Regexp
+        public QuotedStringTokenizer(Options options)
+            : base(options)
         {
-            get { return regexp; }
         }
 
         public QuotedStringTokenizer(char columnDelimiter)
-            : base()
+            : base(new Options('"', '\\', columnDelimiter))
         {
-            BuildCompiledRegexp(columnDelimiter);
-        }
-
-        private void BuildCompiledRegexp(char columnDelimiter)
-        {
-            regexp = new Regex(GetPreparedRegexp(columnDelimiter), RegexOptions.Compiled);
-        }
-
-        private string GetPreparedRegexp(char columnDelimiter)
-        {
-            return string.Format("((?<=\")[^\"]*(?=\"({0}|$)+)|(?<={0}|^)[^{0}\"]*(?={0}|$))", columnDelimiter);
         }
 
         public override string ToString()
