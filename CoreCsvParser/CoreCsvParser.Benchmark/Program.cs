2d59260a29a5cbd2d3d2fee000b7425e929d2d7e
Joel Mueller <joel.mueller@gmail.com>
2018-08-09 23:45:52 -0500

    Better diagnosis of pipline missing-rows issue

--- CoreCsvParser/CoreCsvParser.Benchmark/Program.cs
+++ CoreCsvParser/CoreCsvParser.Benchmark/Program.cs
@@ -12,6 +12,7 @@ namespace CoreCsvParser.Benchmark
 {
     public class LocalWeatherData : IEquatable<LocalWeatherData>
     {
+        public int RowNum { get; set; }
         public string WBAN { get; set; }
         public DateTime Date { get; set; }
         public string SkyCondition { get; set; }
@@ -24,7 +25,8 @@ namespace CoreCsvParser.Benchmark
             if (ReferenceEquals(this, other))
                 return true;
 
-            return WBAN == other.WBAN
+            return RowNum == other.RowNum
+                && WBAN == other.WBAN
                 && Date == other.Date
                 && SkyCondition == other.SkyCondition;
         }
@@ -33,12 +35,12 @@ namespace CoreCsvParser.Benchmark
 
         public override int GetHashCode()
         {
-            return 17 ^ (WBAN?.GetHashCode() ?? 0) ^ Date.GetHashCode() ^ (SkyCondition?.GetHashCode() ?? 0);
+            return 17 ^ RowNum.GetHashCode() ^ (WBAN?.GetHashCode() ?? 0) ^ Date.GetHashCode() ^ (SkyCondition?.GetHashCode() ?? 0);
         }
 
         public override string ToString()
         {
-            return $"{Date}: {WBAN}, {SkyCondition}";
+            return $"{RowNum}: {Date}, {WBAN}, {SkyCondition}";
         }
     }
 
@@ -46,9 +48,10 @@ namespace CoreCsvParser.Benchmark
     {
         public LocalWeatherDataMapper()
         {
-            MapProperty(0, x => x.WBAN);
-            MapProperty(1, x => x.Date, new DateTimeConverter("yyyyMMdd"));
-            MapProperty(4, x => x.SkyCondition);
+            MapProperty(0, x => x.RowNum);
+            MapProperty(1, x => x.WBAN);
+            MapProperty(2, x => x.Date, new DateTimeConverter("yyyyMMdd"));
+            MapProperty(5, x => x.SkyCondition);
         }
     }
 
@@ -99,11 +102,28 @@ namespace CoreCsvParser.Benchmark
 
         public static void CompareLoaders()
         {
-            var csvParserOptions = new CsvParserOptions(true, ',', 4, true);
+            // use this in C# interactive to create subsets of the big file with a specific number of rows
+            //void SubsetFile(string baseFilePath, int rows)
+            //{
+            //    var lines = System.IO.File.ReadLines(baseFilePath)
+            //        .Take(rows + 1)
+            //        .Select((ln, i) => i == 0 ? "RowNum," + ln : $"{i},{ln}");
+
+            //    var path = System.IO.Path.GetDirectoryName(baseFilePath);
+            //    var fileName = System.IO.Path.GetFileNameWithoutExtension(baseFilePath) + "-first-" + rows;
+            //    var ext = System.IO.Path.GetExtension(baseFilePath);
+
+            //    var newPath = System.IO.Path.Combine(path, fileName) + ext;
+
+            //    System.IO.File.WriteAllLines(newPath, lines);
+            //}
+
+            Console.WriteLine("Starting direct file read...");
+            var csvParserOptions = new CsvParserOptions(true, ',', 1, true);
             var csvMapper = new LocalWeatherDataMapper();
             var csvParser = new CsvParser<LocalWeatherData>(csvParserOptions, csvMapper);
 
-            var read = csvParser.ReadFromFile(@"C:\Temp\201503hourly.txt", Encoding.ASCII)
+            var read = csvParser.ReadFromFile(@"C:\Temp\201503hourly-first-1000.txt", Encoding.ASCII)
                 .Select(x =>
                 {
                     if (x.IsValid)
@@ -112,19 +132,23 @@ namespace CoreCsvParser.Benchmark
                         throw x.Error;
                 });
 
-            var readSet = new HashSet<LocalWeatherData>(read);
-            Console.WriteLine($"Read {readSet.Count:N0} distinct lines.");
+            var readItems = new List<LocalWeatherData>(read);
+            Console.WriteLine($"Read {readItems.Count:N0} lines.");
+            Console.WriteLine("Starting Pipeline file read...");
 
-            var pipedSet = new HashSet<LocalWeatherData>(readSet.Count);
-            var observable = csvParser.ObserveFromFile(@"C:\Temp\201503hourly.txt", Encoding.ASCII);
-            observable.Subscribe(x => { pipedSet.Add(x); }, ex => throw ex);
+            var pipedItems = new List<LocalWeatherData>(readItems.Count);
+            var observable = csvParser.ObserveFromFile(@"C:\Temp\201503hourly-first-1000.txt", Encoding.ASCII);
+            observable.Subscribe(x => { pipedItems.Add(x); }, ex => Console.WriteLine(ex.Message));
             observable.ToTask().Wait();
-            Console.WriteLine($"Piped {pipedSet.Count:N0} distinct lines.");
+            Console.WriteLine($"Piped {pipedItems.Count:N0} lines.");
 
-            if (readSet.Count != pipedSet.Count)
+            if (readItems.Count > pipedItems.Count)
             {
-                readSet.SymmetricExceptWith(pipedSet);
-                Console.WriteLine($"Items not in both sets: {readSet.Count:N0}.");
+                Console.WriteLine($"Items missing: {readItems.Count - pipedItems.Count:N0}.");
+                foreach (var item in readItems.Except(pipedItems))
+                {
+                    Console.WriteLine(item.ToString());
+                }
             }
         }
     }
