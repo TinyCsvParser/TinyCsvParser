6dcfa77a79d6eb2ad96c34fc5619846e87d82738
Joel Mueller <joel.mueller@gmail.com>
2018-08-09 21:55:25 -0500

    Work-in-progress analyzing pipline behavior

--- TinyCsvParser/TinyCsvParser.Benchmark/Program.cs
+++ TinyCsvParser/TinyCsvParser.Benchmark/Program.cs
@@ -1,25 +1,44 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Text;
 using TinyCsvParser.Mapping;
 using TinyCsvParser.TypeConverter;
-using System.Reactive;
-using System.Reactive.Threading.Tasks;
-using System.Threading.Tasks;
 
 namespace TinyCsvParser.Benchmark
 {
-    public class LocalWeatherData
+    public class LocalWeatherData : IEquatable<LocalWeatherData>
     {
         public string WBAN { get; set; }
         public DateTime Date { get; set; }
         public string SkyCondition { get; set; }
 
+        public bool Equals(LocalWeatherData other)
+        {
+            if (other is null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return WBAN == other.WBAN
+                && Date == other.Date
+                && SkyCondition == other.SkyCondition;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as LocalWeatherData);
+
+        public override int GetHashCode()
+        {
+            return 17 ^ (WBAN?.GetHashCode() ?? 0) ^ Date.GetHashCode() ^ (SkyCondition?.GetHashCode() ?? 0);
+        }
+
         public override string ToString()
         {
-            return $"{Date}: {SkyCondition}";
+            return $"{Date}: {WBAN}, {SkyCondition}";
         }
     }
 
@@ -46,8 +65,6 @@ namespace TinyCsvParser.Benchmark
             var a = csvParser
                 .ReadFromFile(@"C:\Temp\201503hourly.txt", Encoding.ASCII)
                 .ToList();
-
-            //Console.WriteLine($"Parsed {a.Count:N0} lines.");
         }
 
         [Benchmark]
@@ -63,7 +80,7 @@ namespace TinyCsvParser.Benchmark
         }
 
         [Benchmark]
-        public async Task LocalWeatherPipeline()
+        public void LocalWeatherPipeline()
         {
             // TODO: This method is missing some lines!
             // LocalWeatherRead_One_Core: Parsed 4,496,262 lines.
@@ -73,11 +90,42 @@ namespace TinyCsvParser.Benchmark
             var csvMapper = new LocalWeatherDataMapper();
             var csvParser = new CsvParser<LocalWeatherData>(csvParserOptions, csvMapper);
 
-            int parsedCount = 0;
+            var items = new List<LocalWeatherData>();
             var observable = csvParser.ObserveFromFile(@"C:\Temp\201503hourly.txt", Encoding.ASCII);
-            observable.Subscribe(x => { parsedCount++; }, ex => Console.Error.WriteLine(ex.ToString()));
-            await observable.ToTask();
-            //Console.WriteLine($"Parsed {parsedCount:N0} lines.");
+            observable.Subscribe(items.Add, ex => throw ex);
+            observable.ToTask().Wait();
+            //Console.WriteLine($"Parsed {items.Count:N0} lines.");
+        }
+
+        public static void CompareLoaders()
+        {
+            var csvParserOptions = new CsvParserOptions(true, ',', 4, true);
+            var csvMapper = new LocalWeatherDataMapper();
+            var csvParser = new CsvParser<LocalWeatherData>(csvParserOptions, csvMapper);
+
+            var read = csvParser.ReadFromFile(@"C:\Temp\201503hourly.txt", Encoding.ASCII)
+                .Select(x =>
+                {
+                    if (x.IsValid)
+                        return x.Result;
+                    else
+                        throw x.Error;
+                });
+
+            var readSet = new HashSet<LocalWeatherData>(read);
+            Console.WriteLine($"Read {readSet.Count:N0} distinct lines.");
+
+            var pipedSet = new HashSet<LocalWeatherData>(readSet.Count);
+            var observable = csvParser.ObserveFromFile(@"C:\Temp\201503hourly.txt", Encoding.ASCII);
+            observable.Subscribe(x => { pipedSet.Add(x); }, ex => throw ex);
+            observable.ToTask().Wait();
+            Console.WriteLine($"Piped {pipedSet.Count:N0} distinct lines.");
+
+            if (readSet.Count != pipedSet.Count)
+            {
+                readSet.SymmetricExceptWith(pipedSet);
+                Console.WriteLine($"Items not in both sets: {readSet.Count:N0}.");
+            }
         }
     }
 
@@ -85,10 +133,8 @@ namespace TinyCsvParser.Benchmark
     {
         public static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<CsvBenchmark>();
-            //var bm = new CsvBenchmark();
-            //bm.LocalWeatherRead_One_Core();
-            //bm.LocalWeatherPipeline().Wait();
+            //var summary = BenchmarkRunner.Run<CsvBenchmark>();
+            CsvBenchmark.CompareLoaders();
         }
     }
 }
