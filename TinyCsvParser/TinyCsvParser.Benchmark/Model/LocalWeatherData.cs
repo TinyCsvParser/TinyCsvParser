b1d6d22279b4736a41f8624bf204682a72be2884
Philipp Wagner <pwag86@googlemail.com>
2019-04-14 15:39:18 +0200

    Add Benchmarks

---
+++ TinyCsvParser/TinyCsvParser.Benchmark/Model/LocalWeatherData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TinyCsvParser.Benchmark.Model
+{
+    public class LocalWeatherData
+    {
+        public string WBAN { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public TimeSpan Time { get; set; }
+
+        public string SkyCondition { get; set; }
+
+        public float DryBulbCelsius { get; set; }
+
+        public float WindSpeed { get; set; }
+
+        public float StationPressure { get; set; }
+    }
+}
