c7eed0f32331558c4845a6fda4ed914b3c28bdf6
djgrech <39998811+djgrech@users.noreply.github.com>
2024-02-22 08:47:42 +0100

    reformatting

--- TinyCsvParser/TinyCsvParser.Test/CsvParser/CsvParserTest.cs
+++ TinyCsvParser/TinyCsvParser.Test/CsvParser/CsvParserTest.cs
@@ -149,7 +149,7 @@ namespace TinyCsvParser.Test.CsvParser
 
             var result = csvParser
                 .ReadFromString(csvReaderOptions, stringBuilder.ToString())
-                        .result
+                .result
                 .ToList();
 
             Assert.AreEqual(0, result.Count);
