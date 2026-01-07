ef5f7d0d307b6775123aa9e02abc532cc5aefe58
Externius <t.andras@outlook.com>
2026-01-07 18:01:01 +0100

    Fix failing unit tests

--- TinyCsvParser/TinyCsvParser.Test/TypeConverter/NullableDoubleConverterTest.cs
+++ TinyCsvParser/TinyCsvParser.Test/TypeConverter/NullableDoubleConverterTest.cs
@@ -8,64 +8,49 @@ using TinyCsvParser.TypeConverter;
 namespace TinyCsvParser.Test.TypeConverter
 {
     [TestFixture]
-    public class NullableDoubleConverterTest : BaseConverterTest<Double?>
+    public class NullableDoubleConverterTest : BaseConverterTest<double?>
     {
-        protected override ITypeConverter<Double?> Converter
-        {
-            get { return new NullableDoubleConverter(); }
-        }
+        protected override ITypeConverter<double?> Converter => new NullableDoubleConverter();
 
-        protected override Tuple<string, Double?>[] SuccessTestData
-        {
-            get
-            {
-                return new[] {
-                    MakeTuple(double.MinValue.ToString("R"), double.NegativeInfinity),
-                    MakeTuple(double.MaxValue.ToString("R"), double.PositiveInfinity),
-                    MakeTuple("0", 0),
-                    MakeTuple("-1000", -1000),
-                    MakeTuple("1000", 1000),
-                    MakeTuple("5e2", 500),
-                    MakeTuple(" ", default(Double?)),
-                    MakeTuple(null, default(Double?)),
-                    MakeTuple(string.Empty, default(Double?))
-                };
-            }
-        }
+        protected override Tuple<string, double?>[] SuccessTestData =>
+        [
+            MakeTuple(double.MinValue.ToString("R"), double.MinValue),
+            MakeTuple(double.MaxValue.ToString("R"), double.MaxValue),
+            MakeTuple("0", 0),
+            MakeTuple("-1000", -1000),
+            MakeTuple("1000", 1000),
+            MakeTuple("5e2", 500),
+            MakeTuple(" ", null),
+            MakeTuple(null, null),
+            MakeTuple(string.Empty, null)
+        ];
 
-        public override void AssertAreEqual(double? expected, double? actual)
+        protected override void AssertAreEqual(double? expected, double? actual)
         {
-            if (expected == default(double?))
+            if (expected is null)
             {
-                Assert.AreEqual(expected, actual);
+                Assert.AreEqual(null, actual);
             }
             else
             {
-                Assert.That(actual.Value, Is.EqualTo(expected.Value).Within(double.Epsilon));
+                NUnit.Framework.Assert.That(actual, Is.EqualTo(expected.Value).Within(double.Epsilon));
             }
         }
 
-        protected override string[] FailTestData
-        {
-            get { return new[] { "a", }; }
-        }
+        protected override string[] FailTestData => ["a"];
     }
 
     [TestFixture]
     public class NullableDoubleConverterWithFormatProviderTest : NullableDoubleConverterTest
     {
-        protected override ITypeConverter<Double?> Converter
-        {
-            get { return new NullableDoubleConverter(CultureInfo.InvariantCulture); }
-        }
+        protected override ITypeConverter<double?> Converter =>
+            new NullableDoubleConverter(CultureInfo.InvariantCulture);
     }
 
     [TestFixture]
     public class NullableDoubleConverterWithFormatProviderAndNumberStyleTest : NullableDoubleConverterTest
     {
-        protected override ITypeConverter<Double?> Converter
-        {
-            get { return new NullableDoubleConverter(CultureInfo.InvariantCulture, NumberStyles.Float | NumberStyles.AllowThousands); }
-        }
+        protected override ITypeConverter<double?> Converter =>
+            new NullableDoubleConverter(CultureInfo.InvariantCulture, NumberStyles.Float | NumberStyles.AllowThousands);
     }
 }
--- TinyCsvParser/TinyCsvParser.Test/TypeConverter/SingleConverterTest.cs
+++ TinyCsvParser/TinyCsvParser.Test/TypeConverter/SingleConverterTest.cs
@@ -7,36 +7,25 @@ using TinyCsvParser.TypeConverter;
 namespace TinyCsvParser.Test.TypeConverter
 {
     [TestFixture]
-    public class SingleConverterTest : BaseConverterTest<Single>
+    public class SingleConverterTest : BaseConverterTest<float>
     {
-        protected override ITypeConverter<Single> Converter
+        protected override ITypeConverter<float> Converter => new SingleConverter();
+
+        protected override Tuple<string, float>[] SuccessTestData =>
+        [
+            MakeTuple(float.MinValue.ToString("R"), float.MinValue),
+            MakeTuple(float.MaxValue.ToString("R"), float.MaxValue),
+            MakeTuple("0", 0),
+            MakeTuple("-1000", -1000),
+            MakeTuple("1000", 1000),
+            MakeTuple("5e2", 500)
+        ];
+
+        protected override void AssertAreEqual(float expected, float actual)
         {
-            get { return new SingleConverter(); }
+            NUnit.Framework.Assert.That(actual, Is.EqualTo(expected).Within(float.Epsilon));
         }
 
-        protected override Tuple<string, Single>[] SuccessTestData
-        {
-            get
-            {
-                return new[] {
-                    MakeTuple(float.MinValue.ToString("R"), float.NegativeInfinity),
-                    MakeTuple(float.MaxValue.ToString("R"), float.PositiveInfinity),
-                    MakeTuple("0", 0),
-                    MakeTuple("-1000", -1000),
-                    MakeTuple("1000", 1000),
-                    MakeTuple("5e2", 500),
-                };
-            }
-        }
-
-        public override void AssertAreEqual(float expected, float actual)
-        {
-            Assert.That(actual, Is.EqualTo(expected).Within(float.Epsilon));
-        }
-
-        protected override string[] FailTestData
-        {
-            get { return new[] { "a", string.Empty, "  ", null }; }
-        }
+        protected override string[] FailTestData => ["a", string.Empty, "  ", null];
     }
 }
