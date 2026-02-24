7861b49b1a063e8957cce9b49e6f519250935f68
Externius <t.andras@outlook.com>
2026-02-24 20:20:36 +0100

    Remove unused usings

--- TinyCsvParser/TinyCsvParser.Test/CsvParser/EnumConverterTest.cs
+++ TinyCsvParser/TinyCsvParser.Test/CsvParser/EnumConverterTest.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using NUnit.Framework;
-using System;
 using System.Linq;
 using System.Text;
 using TinyCsvParser.Models;
